de39a2176afb014041548ea022845125428de4db
Youssef Mahmoud <114652507+Yousseff-2@users.noreply.github.com>
2025-07-08 23:00:14 +0300

    Delete E-commerce directory

--- E-commerce/src/main/java/com/Ecommerce/Product/Expirable.java
+++
@@ -1,5 +0,0 @@
-package com.Ecommerce.Product;
-
-public interface Expirable {
-    public boolean isExpired();
-}
--- E-commerce/src/main/java/com/Ecommerce/Product/Shippable.java
+++
@@ -1,6 +0,0 @@
-package com.Ecommerce.Product;
-
-public interface Shippable {
-    double getWeight();
-    String getName();
-}
